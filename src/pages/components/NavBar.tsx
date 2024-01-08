4077ac94530b2f79b9c6a2ecc332d0307c59daa4
kitkatmia <alinka.markova@gmail.com>
2024-01-07 17:52:52 -0800

    navbar changes

--- src/pages/components/NavBar.tsx
+++ src/pages/components/NavBar.tsx
@@ -3,9 +3,12 @@ import * as React from 'react';
 import Button from '@mui/joy/Button';
 import ButtonGroup from '@mui/joy/ButtonGroup';
 import Link from 'next/link';
-// import { Link } from 'react-router-dom';
+import { useSession } from 'next-auth/react';
+import { IconButton } from '@mui/material';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 export default function NavBar() {
+    const { data: session } = useSession();
     return (
     <div className='flex justify-between p-4'>
         <Link href="/">MVLA Lab Support</Link>
@@ -20,8 +23,15 @@ export default function NavBar() {
                 <a href="https://sites.google.com/mvla.net/mvlasciencelabtech/" target='_blank'>
                     <Button>General Info</Button>
                 </a>
-            </ButtonGroup>
-             <Button className='ml-5' variant="outlined" color="success">Sign-in</Button>
+                </ButtonGroup>
+                {session ? (
+                    <IconButton aria-label="settings">
+                        <SettingsIcon />
+                    </IconButton>
+                ) : (
+                    <Button className='ml-5' variant="outlined" color="success">Sign-in</Button>
+                )}
+             
         </div>
     </div>
 
