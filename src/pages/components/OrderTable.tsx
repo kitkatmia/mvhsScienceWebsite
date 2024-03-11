0c7c26313ca0f7fbd8fd238b585b2c5cfa66ee33
ThePersonOfStuff <maxf6.283@gmail.com>
2024-03-11 10:17:19 -0700

    comment adding

--- src/pages/components/OrderTable.tsx
+++ src/pages/components/OrderTable.tsx
@@ -3,6 +3,11 @@
 import type { Order, Comment, User, Status } from "@prisma/client";
 import { useState } from "react";
 import Image from "next/image";
+import {
+  Button,
+  ClickAwayListener,
+  TextareaAutosize,
+} from "@mui/material";
 
 const statusMap: Record<Status, string> = {
   Complete: "Complete",
@@ -19,9 +24,9 @@ type OrderWithCommentsAndUser = Order & {
 export default function OrderTable(props: {
   orders: OrderWithCommentsAndUser[];
 }) {
-    if(props.orders == undefined) {
-        props.orders = []
-    }
+  if (props.orders == undefined) {
+    props.orders = [];
+  }
   return (
     <table className="w-full table-auto border-collapse">
       <thead>
@@ -73,21 +78,47 @@ function CommentBox(data: { comments: (Comment & { user: User })[] }) {
 
   return (
     <>
-      <span
-        className="cursor-pointer rounded-md bg-blue-500 p-1"
-        onClick={() => setOpened(!opened)}
+      <Button
+        className="cursor-pointer rounded-md border border-solid border-blue-200 p-2"
+        onClick={() => setTimeout(() => setOpened(!opened), 1)}
       >
         {data.comments.length + " Comments"}
-      </span>
-      {opened && (
-        <div className="absolute rounded-lg border-[2px] border-solid border-blue-500 bg-slate-900">
-          {data.comments.map((e) => (
-            <div key={e.id}>
-              {e.user.image && <Image src={e.user.image} alt="user image" width={100} height={100}/>}{e.user.name + ": "} <span className="text-sm">{e.contents}</span>
+      </Button>
+      <ClickAwayListener onClickAway={() => setOpened(false)}>
+        {opened ? (
+          <div className="absolute rounded-lg border-[2px] border-solid border-blue-500 bg-slate-900">
+            {data.comments.map((e) => (
+              <div key={e.id}>
+                {e.user.image && (
+                  <Image
+                    src={e.user.image}
+                    alt="user image"
+                    width={50}
+                    height={50}
+                    className="rounded-full"
+                  />
+                )}
+                {e.user.name + ": "}{" "}
+                <span className="text-sm">{e.contents}</span>
+              </div>
+            ))}
+            <div className="m-2 flex">
+              <TextareaAutosize
+                className="m-2 resize-none flex-grow"
+                placeholder="Enter a comment..."
+              />
+              <Button
+                type="submit"
+                className="border-[1px] border-solid border-blue-200 m-2"
+              >
+                Post
+              </Button>
             </div>
-          ))}
-        </div>
-      )}
+          </div>
+        ) : (
+          <></>
+        )}
+      </ClickAwayListener>
     </>
   );
 }
