7ead92f804ff724ddbb0af4649fe9bd26b5ecc9d
ThePersonOfStuff <maxf6.283@gmail.com>
2024-03-17 14:20:25 -0700

    things

--- src/pages/order_status.tsx
+++ src/pages/order_status.tsx
@@ -3,9 +3,8 @@ import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import PageTitle from "./components/PageTitle";
 import OrderTable from "./components/OrderTable";
-// import { useSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import type { Order, User, Comment } from "@prisma/client";
-import prismaClient from "~/utils/prismaClient";
 import { api } from "~/utils/api";
 
 type OrderWithCommentsAndUser = Order & {
@@ -13,47 +12,9 @@ type OrderWithCommentsAndUser = Order & {
   user: User | undefined;
 };
 
-// const johnDoe: User = {
-//   id: "asdqwe",
-//   name: "John Doe",
-//   email: null,
-//   emailVerified: null,
-//   image:
-//     "https://lh3.googleusercontent.com/a/ACg8ocKzngrO7d0VOOFnlqkAkP8YQ6I6yKsoFfG-7-ZQ9VVFWg=s96-c",
-//   location: null,
-//   role: null,
-//   school: null,
-// };
-// const orders: OrderWithCommentsAndUser[] = [
-//   {
-//     id: "ashgdjhfkjafhkj",
-//     userId: johnDoe.id,
-//     user: johnDoe,
-//     date: new Date(1),
-//     status: "In_Progress",
-//     comments: [
-//       {
-//         id: "asdfw",
-//         contents: "Here Have a Comment",
-//         orderId: "ashgdjhfkjafhkj",
-//         userId: johnDoe.id,
-//         user: johnDoe,
-//       },
-//       {
-//         id: "asddasd",
-//         contents: "Here Have another Comment",
-//         orderId: "ashgdjhfkjafhkj",
-//         userId: johnDoe.id,
-//         user: johnDoe,
-//       },
-//     ],
-//     categories: "Chem, Lab",
-//     description: "Setup electrochem lab",
-//   },
-// ];
 
 export default function OrderStatus() {
-  // const session = useSession();
+  const { data: sessionData } = useSession();
   const emptyOrders: OrderWithCommentsAndUser[] = [];
   const [orders, setOrders] = useState(emptyOrders);
 
@@ -66,7 +27,6 @@ export default function OrderStatus() {
     <>
       <NavBar />
       <PageTitle title="Order Status" />
-      {/* {<div>{(sessionData?.user.role == 1) ? "user view": <OrderTable orders={orders}/> "admin view"}</div>} */}
       <OrderTable orders={orders} />
       <Footer />
     </>
