f42fecd22dfd049f626b63b7ca2f62299e70ea82
ThePersonOfStuff <maxf6.283@gmail.com>
2024-04-26 08:26:12 -0700

    ASD

--- src/pages/order_status.tsx
+++ src/pages/order_status.tsx
@@ -13,31 +13,40 @@ type OrderWithCommentsAndUser = Order & {
   user: User | undefined;
 };
 
+const generateSearchFilter = (searchTerm: string) => (e: OrderWithCommentsAndUser) => {
+  if(searchTerm.length == 0) {
+    return true;
+  }
+  console.log("matching");
+    if(e.categories.includes(searchTerm)) {
+      console.log(e.categories + " includes " + searchTerm);
+      return true;
+    }
+    if(e.comments?.filter((comment) => (comment.contents.includes(searchTerm))) && e.comments?.filter((comment) => (comment.contents.includes(searchTerm))).length > 0) {
+      console.log(e.comments + " inncludes " + searchTerm);
+      return true;
+    }
+    if(e.description.includes(searchTerm)) {
+      console.log(e.description + " includes " + searchTerm);
+      return true;
+    }
+    if(JSON.stringify(e.details).includes(searchTerm)) {
+      console.log("Details contain " + searchTerm);
+      return true;
+    }
+    console.log("matchless");
+    return false;
+}
 
 export default function OrderStatus() {
   const { data: sessionData } = useSession();
   const [orders, setOrders] = useState<OrderWithCommentsAndUser[]>([]);
   const [search, setSearch] = useState("");
-  const [filters, setFilters] = useState<((e: OrderWithCommentsAndUser) => boolean)[]>([(e) => {
-    if(e.categories.includes(search)) {
-      return true;
-    }
-    if(e.comments?.filter((comment) => (comment.contents.includes(search)))) {
-      return true;
-    }
-    if(e.description.includes(search)) {
-      return true;
-    }
-    if(e.details?.toString() != "[Object object]" && e.details?.toString().includes(search)) {
-      return true;
-    }
-    return false;
-  }]);
+  const [filters, setFilters] = useState<((e: OrderWithCommentsAndUser) => boolean)[]>([generateSearchFilter(search)]);
 
   const orderQuery = api.order.getOrders.useQuery();
   useEffect(() => {
     setOrders(orderQuery.data? orderQuery.data : []);
-    console.log(orders);
   });
   
   return (
@@ -46,8 +55,9 @@ export default function OrderStatus() {
       <PageTitle title="Order Status" />
       <TextField fullWidth className="m-4" label="Search" value={search} onChange={(e) => {
         setSearch(e.target.value)
+        setFilters([generateSearchFilter(search)]);
       }}/>
-      {(sessionData?.user.role == 1) ? "user view": <OrderTable orders={orders} filters={[]} />}
+      {(sessionData?.user.role == 1) ? "user view": <OrderTable orders={orders} filters={filters} />}
       <Footer />
     </>
   );
